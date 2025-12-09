c05b72f60551f02ef9fde7b278b6495b81b3a37d
EsinShadrach <esinshadrachrafe@gmail.com>
2025-12-10 00:54:39 +0100

    Our Apps

--- app/_components/our-apps.tsx
+++ app/_components/our-apps.tsx
@@ -13,21 +13,21 @@ import Image from "next/image";
 
 export function OurApps() {
   return (
-    <section id="our-apps" className="min-h-screen mt-36">
-      <div className="container mx-auto p-3 flex justify-between">
+    <section id="our-apps" className="min-h-screen md:mt-36 mt-14">
+      <div className="container mx-auto p-3 flex justify-between md:flex-row flex-col">
         <div className="space-y-8">
-          <div className="px-6 py-3 bg-light-grey rounded-3xl w-fit">
-            <div className="text-light-black text-lg font-normal capitalize">
+          <div className="px-6 py-3 bg-light-grey rounded-3xl w-fit max-md:mx-auto">
+            <div className="text-light-black text-sm md:text-lg font-normal capitalize">
               our products
             </div>
           </div>
-          <div className="justify-start text-Light-Black text-7xl font-normal capitalize leading-25">
-            what we have <br />
+          <div className="justify-start text-Light-Black text-4xl md:text-7xl font-normal capitalize md:leading-25 max-md:text-center leading-9">
+            what we have <br className="max-md:hidden" />
             built so far
           </div>
-          <div className="justify-start text-dark-grey text-2xl font-normal capitalize leading-9 -mt-8">
-            These are the solutions we’re developing for
-            <br />
+          <div className="justify-start text-dark-grey text-xl md:text-2xl max-md:my-4 max-md:mb-10 font-normal capitalize md:leading-9 md:-mt-8 max-md:text-center">
+            These are the solutions we&apos;re developing for{" "}
+            <br className="max-md:hidden" />
             our users.
           </div>
         </div>
@@ -51,8 +51,6 @@ export function OurApps() {
               </div>
               <div>
                 <div className="h-58 w-full relative">
-                  <_Star image={person1} />
-
                   <div
                     className="absolute inset-0 blur-xl"
                     style={{
@@ -62,19 +60,19 @@ export function OurApps() {
                   />
                   <div className="absolute inset-0">
                     <div className="px-6 py-3 bg-light-grey rounded-3xl w-fit mx-auto">
-                      <div className="text-light-black text-sm font-normal capitalize flex justify-center gap-2 items-center">
+                      <div className="text-dark-grey text-sm font-normal capitalize flex justify-center gap-2 items-center">
                         <span>
-                          <Profile2User className="text-light-black" />
+                          <Profile2User className="text-dark-grey max-md:size-5" />
                         </span>
                         <span>10,000+ daily users</span>
                       </div>
                     </div>
                     <div className="mt-5 flex flex-col justify-center">
-                      <div className="text-center justify-start text-light-black text-5xl font-bold capitalize">
+                      <div className="text-center justify-start text-light-black md:text-5xl text-3xl font-bold capitalize">
                         Predict smarter,
                       </div>
-                      <div className="px-12 py-2.5 origin-top-left rotate-[3.14deg] bg-Blue inline-flex justify-center items-center gap-2.5 bg-primary mx-auto">
-                        <div className="text-center justify-start text-grey-1 text-5xl font-bold capitalize py-2">
+                      <div className="px-12 py-1 md:py-2.5 origin-top-left rotate-[3.14deg] bg-Blue inline-flex justify-center items-center gap-2.5 bg-primary mx-auto">
+                        <div className="text-center justify-start text-grey-1 md:text-5xl text-3xl font-bold capitalize py-2">
                           win bigger
                         </div>
                       </div>
@@ -82,34 +80,34 @@ export function OurApps() {
                   </div>
                 </div>
               </div>
-              <div className="text-center text-dark-grey text-xl font-normal mt-6 capitalize">
+              <div className="text-center text-dark-grey text-base md:text-xl font-normal mt-6 capitalize">
                 Puntrr uses AI to analyze stats, form, and performance trends
                 helping you bet with strategy, not emotion.
               </div>
-              <div className="flex gap-6 mt-12 w-fit mx-auto">
+              <div className="flex gap-6 mt-12 md:w-fit mx-auto md:flex-row flex-col max-md:px-10">
                 <PrimaryButton
                   icon={<ReceiveSquare />}
                   label="Download Now"
-                  className="text-background"
+                  className="text-background md:text-lg text-base max-md:w-full"
                 />
                 <PrimaryButton
                   icon={<Logout />}
                   label="Visit Website"
-                  className="text-light-black bg-light-grey! hover:bg-grey-1!"
+                  className="text-light-black bg-light-grey! hover:bg-grey-1! md:text-lg text-base max-md:w-full"
                 />
               </div>
-              <div className="text-center text-dark-grey text-lg font-normal capitalize mt-6">
+              <div className="text-center text-dark-grey text-base md:text-lg font-normal capitalize mt-6">
                 available on appstore and playstore
               </div>
             </div>
           </div>
           <div className="border border-dark-grey rounded-4xl py-6 px-10 mt-6">
             <div className="flex justify-between items-center">
-              <div className="">
-                <span className="text-light-black text-4xl font-normal capitalize">
+              <div className="md:text-4xl text-2xl font-normal capitalize">
+                <span className="text-light-black font-normal capitalize">
                   pustats{" "}
                 </span>
-                <span className="text-dark-grey text-4xl font-normal capitalize">
+                <span className="text-dark-grey font-normal capitalize">
                   (coming soon)
                 </span>
               </div>
@@ -119,17 +117,5 @@ export function OurApps() {
         </div>
       </div>
     </section>
-  );
-}
-
-function _Star({ image }: { image: StaticImport }) {
-  return (
-    <div className="size-21 flex justify-center relative z-10">
-      <Star2 />
-      <div className="absolute inset-0 flex justify-center items-center">
-        <Image alt="person" src={image} />
-      </div>
-      <ArrowCursor className="absolute -bottom-3 rotate-10 right-2" />
-    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
 
       <OurApps />
 
-      {/* <PlaycopeVision />
+      {/* <PlaycopeVision /> */}
 
-      <Testimonials />
+      {/* <Testimonials />
 
       <AboutUs />
 
